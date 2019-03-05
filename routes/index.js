782d07cd289dae45f766e345281df964322dc9d1
0blivious <lyw@415309@gmail.com>
2019-03-05 13:27:26 +0800

    login admin 123456

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        message : req.flash('error').toString(),
+        isLoggedIn: req.user
+    });
 });
+
+router.get('/login',function (req, res, next) {
+    res.render('login', {
+        title : 'Login Page'
+    })
+});
+
+router.post('/login',
+    passport.authenticate('local',{
+        successRedirect: '/dashboard',
+        failureRedirect: '/',
+        failureFlash : true
+    })
+);
+
+router.get('/register',function (req, res, next) {
+    res.render('register', {
+        title : 'Register Page'
+    })
+});
+
+router.all('/dashboard', isLoggedIn);
+router.get('/dashboard', function(req, res, next) {
+   res.render('dashboard',{
+        title : 'User List',
+        username : req.user.username
+    })
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
 
 module.exports = router;
