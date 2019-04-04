a1921222b0d2063441c050a6827dca3346b46e42
0blivious <lyw@415309@gmail.com>
2019-04-04 21:23:47 +0800

    suppport payment from owner

--- postgresql_scripts/index.js
+++ postgresql_scripts/index.js
@@ -10,12 +10,12 @@ sql.query = {
 	// Caretakers
 	my_avail: 'SELECT * FROM Availabilities WHERE ctname=$1 ORDER BY start_ts ASC',
 	single_avail_bids: 'SELECT * FROM Bids WHERE availabilityId=$1 and id not in (SELECT id from AcceptedBids)',
-	ct_accepted_bids: 'SELECT Bids.oname as oname, Bids.ostart_ts as start, Bids.oend_ts as end, Bids.bidded_price_per_hour as price FROM Availabilities inner join Bids on Availabilities.id = Bids.availabilityId WHERE Availabilities.ctname = $1 and Bids.id in (SELECT id FROM AcceptedBids)',
+	ct_accepted_bids: 'SELECT Bids.id as id, Bids.oname as oname, Bids.ostart_ts as start, Bids.oend_ts as end, Bids.bidded_price_per_hour as price FROM Availabilities inner join Bids on Availabilities.id = Bids.availabilityId WHERE Availabilities.ctname = $1 and Bids.id in (SELECT id FROM AcceptedBids)',
 
 
 	// Owners
 	placed_bids: 'SELECT Availabilities.ctname as ctname, Bids.ostart_ts as start, Bids.oend_ts as end, Bids.bidded_price_per_hour as bidded_price_per_hour FROM Availabilities inner join Bids on Availabilities.id = Bids.availabilityId WHERE Bids.oname = $1',
-	owner_accepted_bids: 'SELECT Availabilities.ctname as ctname, Bids.ostart_ts as start, Bids.oend_ts as end, Bids.bidded_price_per_hour as price FROM Availabilities inner join Bids on Availabilities.id = Bids.availabilityId WHERE Bids.oname = $1 and Bids.id in (SELECT id FROM AcceptedBids)',
+	owner_accepted_bids: 'SELECT Bids.id as id, Availabilities.ctname as ctname, Bids.ostart_ts as start, Bids.oend_ts as end, Bids.bidded_price_per_hour as price FROM Availabilities inner join Bids on Availabilities.id = Bids.availabilityId WHERE Bids.oname = $1 and Bids.id in (SELECT id FROM AcceptedBids) and Bids.id not in (SELECT id from Payments)',
 	owner_pets: 'SELECT pname, gender, species, weight_class FROM Pets where oname=$1',
 	
 	// Insertion
@@ -25,6 +25,10 @@ sql.query = {
 	add_availability: 'INSERT INTO Availabilities (id, ctname, start_ts, end_ts) VALUES ((SELECT (COALESCE(MAX(id), 0)) from Availabilities)+1,$1,$2,$3)',
 	add_bid: 'INSERT INTO Bids (id, availabilityid, oname,ostart_ts, oend_ts, bidded_price_per_hour) VALUES ((SELECT (COALESCE(MAX(id), 0)) from Bids)+1, $1,$2,$3,$4,$5)',
 	accept_bid: 'INSERT INTO AcceptedBids (id) VALUES ($1)',
+	add_payment: 'INSERT INTO Payments (payment_id, id, value) VALUES ((SELECT (COALESCE(MAX(payment_id), 0)) from Payments)+1, $1,$2)',
+
+	// UpsRW
+	update_acceptedBid_owner: 'UPDATE AcceptedBids SET ctrating=($1), ocomments=($2) WHERE id=($3)',
 
 	// Login
 	userpass: 'SELECT * FROM Users WHERE username=$1',
